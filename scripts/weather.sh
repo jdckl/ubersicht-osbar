b7647fb6f2e7a02231c76113c4c9959cd04b8909
Jan Dočekal <hello@jdckl.dev>
2021-07-09 21:40:11 +0200

    Index update weather if condition

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CITY='Prague,CZ'
-APIKEY='4f75c0af3c70963adad7a05f811994a9'
+APIKEY=''
 
 echo $(curl -s --request GET \
      --url "api.openweathermap.org/data/2.5/weather?q=${CITY}&units=metric&appid=${APIKEY}" \
